c145be2d5ed412422ca36f54edcc9a380034aa27
Jay Govindan <jay_gj@yahoo.com>
2012-04-17 22:54:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyPhoto::Application.config.secret_token = 'c434764c9595114a6e88ea2cb149109198c59cd758b6c238e3519b8c589bd39fbe59b283b6a25d986f53a7cc786ce17fa74d22a835aafac3ffffbb6a25b1a8d7'
